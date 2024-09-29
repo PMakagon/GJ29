c22be4bb0556fdfa0c21bf6b0560b8235c9d3414
PMakagon <haggard95@yandex.ru>
2024-09-29 23:03:31 +0300

    Room,Events,LineConnectors

---
+++ Assets/_Project/Develop/StunGames/GameJam29/Runtime/EventHolder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _Project.Develop.StunGames.GameJam29.Runtime
+{
+    public static class EventHolder
+    {
+        public static event Action<Room> OnPlayerEnterRoom = delegate { };
+        public static event Action<Room> onPlayerExitRoom = delegate { };
+        public static event Action<ItemType> OnPlayerUseItem = delegate { };
+        
+        public static void RaisePlayerEnterRoom(Room roomToEnter)
+        {
+            OnPlayerEnterRoom?.Invoke(roomToEnter);
+        }
+        public static void RaisePlayerExitRoom(Room roomToExit)
+        {
+            onPlayerExitRoom?.Invoke(roomToExit);
+        }
+        
+        public static void RaisePlayerUseItem(ItemType itemType)
+        {
+            OnPlayerUseItem?.Invoke(itemType);
+        }
+        
+    }
+}
