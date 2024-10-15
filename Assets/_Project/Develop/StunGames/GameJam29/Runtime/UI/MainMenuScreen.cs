1cde2c897e128d92803070c36d5d41e8068a99f9
PMakagon <haggard95@yandex.ru>
2024-10-15 10:26:19 +0300

    ADDED mainMenu debug section

--- Assets/_Project/Develop/StunGames/GameJam29/Runtime/UI/MainMenuScreen.cs
+++ Assets/_Project/Develop/StunGames/GameJam29/Runtime/UI/MainMenuScreen.cs
@@ -7,6 +7,8 @@ namespace _Project.Develop.StunGames.GameJam29.Runtime.UI
     public class MainMenuScreen : UIPanel
     {
         [SerializeField] private Button playBtn;
+        [SerializeField] private Toggle showMonsterBtn;
+        [SerializeField] private GameConfig gameConfig;
         private MainMenuFlow _mainMenuFlow;
 
         public void Init(MainMenuFlow mainMenuFlow)
@@ -18,6 +20,12 @@ namespace _Project.Develop.StunGames.GameJam29.Runtime.UI
         protected override void BindButtons()
         {
             playBtn.onClick.AddListener(_mainMenuFlow.Play);
+            showMonsterBtn.onValueChanged.AddListener(ToggleMonsterShow);
+        }
+
+        private void ToggleMonsterShow(bool isTrue)
+        {
+            gameConfig.alwaysShowMonster = isTrue;
         }
         protected override void RemoveAllListeners()
         {
